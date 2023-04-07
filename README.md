4a927807486c84532887cc522550ffdff28b548d
Michael Ross <michael.ross.contact@gmail.com>
2023-04-07 15:59:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Etch-a-sketch
+ Etch-a-sketch clone made using HTML, CSS and JS
---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  container.appendChild(row);
+  for (let j = 0; j < 16; j++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    row.appendChild(cell);
+  }
+}
